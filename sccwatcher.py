939d6cf400d9e086ed40923ab8ab9a625e506fc0
T <banana@kylesplace.org>
2015-11-26 20:50:19 -0800

    Bug fixes
    Some more bugs found and squashed. This time there were issues with the
    app not asking to save changes when closed via certain methods. It also
    used to ignore user canceling inside of a save-file dialog. Plus some
    other stuff I cant remember now.

--- sccwatcher.py
+++ sccwatcher.py
@@ -7,14 +7,25 @@
 
 import sys
 from settings_ui import *
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
+#This is required to override the closeEvent
+class SCCMainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        super(SCCMainWindow, self).__init__(parent)
+        self._user_accept_close = False
 
-
+    def closeEvent(self, event):
+        #We first emit the closing signal, then we actually close
+        self.emit(QtCore.SIGNAL("appClosing"))
+        if self._user_accept_close is True:
+            super(SCCMainWindow, self).closeEvent(event)
+        else:
+            event.ignore()
 
 def main():
     app = QtGui.QApplication(sys.argv)
-    Window = QtGui.QMainWindow()
+    Window = SCCMainWindow()
     ui = Ui_sccw_SettingsUI()
     ui.setupUi(Window)
     Window.show()
